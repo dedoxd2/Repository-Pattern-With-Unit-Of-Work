3995388780684b104f87cdd40899e65cf739ae1b
dedoxd2 <dedo.xd33@yahoo.com>
2024-04-20 00:21:25 +0200

    project initializing

--- RepositoryPatternWithUnitOfWork/RepositoryPatternWithUnitOfWork.Api/Program.cs
+++ RepositoryPatternWithUnitOfWork/RepositoryPatternWithUnitOfWork.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using RepositoryPatternWithUOfW.EF;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+       options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+       b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+
 
 var app = builder.Build();
 
